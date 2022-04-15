899ba364e3662ad9908b9c487dda8861b248e87c
koyamablackcat <lonelyblackcat95@gmail.com>
2022-04-15 21:57:23 +0700

    first commit

---
+++ menu.js
@@ -0,0 +1,9 @@
+// Script to open and close sidebar
+function menuOpen() {
+    document.getElementById("mySidebar").style.display = "flex";
+    console.log("test");
+  }
+
+function menuClose() {
+    document.getElementById("mySidebar").style.display = "none";
+  }
